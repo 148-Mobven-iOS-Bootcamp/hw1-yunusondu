cad66f2fd268e740264e955fddc3c8eb1373dea0
yunusondu <yunusondu@gmail.com>
2021-12-16 23:30:03 +0300

    fibonacci added

---
+++ Fibonacci.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var a = 0, b = 1, sonuc = 0
+  
+for  _ in stride(from: 1, through: 20, by: 1){
+    a = b
+    b = sonuc
+    sonuc = a + b
+    if  sonuc > 750 && sonuc < 1000{
+        print(sonuc)
+    }
+}
